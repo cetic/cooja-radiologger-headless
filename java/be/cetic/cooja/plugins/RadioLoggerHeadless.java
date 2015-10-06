23a0de4adabbe4b773a532a9348218f7ed82cb8e
Yasuyuki Tanaka <yasuyuki.tanaka@inf.ethz.ch>
2015-10-06 15:05:55 +0200

    Replace "RadioMediumObserver" with "RadioTransmissionObserver"
    You fail to build cooja-radiologger-headless under the master branch
    of contiki because of the refactoring of RadioMedium commited to
    contiki/master on Aug 31, 2015. The short hash of the commit is
    c1a275f.
    
    Two methods on which cooja-radiologger-headless depends,
    "addRadioMediumObserver" and "deleteRadioMediumObserver, are
    obsolete. They was changed to "addRadioTransmissionObserver" and
    "deleteRadioTransmissionObserver" by the commit.
    
    In order to follow the changes, the necessary string replacement was
    made.

--- java/be/cetic/cooja/plugins/RadioLoggerHeadless.java
+++ java/be/cetic/cooja/plugins/RadioLoggerHeadless.java
@@ -83,7 +83,7 @@ public class RadioLoggerHeadless extends VisPlugin {
     simulation = simulationToControl;
     radioMedium = simulation.getRadioMedium();
 
-    radioMedium.addRadioMediumObserver(radioMediumObserver = new Observer() {
+    radioMedium.addRadioTransmissionObserver(radioMediumObserver = new Observer() {
       public void update(Observable obs, Object obj) {
         RadioConnection conn = radioMedium.getLastConnection();
         if (conn == null) {
@@ -102,7 +102,7 @@ public class RadioLoggerHeadless extends VisPlugin {
 
   public void closePlugin() {
     if (radioMediumObserver != null) {
-      radioMedium.deleteRadioMediumObserver(radioMediumObserver);
+      radioMedium.deleteRadioTransmissionObserver(radioMediumObserver);
     }
   }
   public boolean setConfigXML(Collection<Element> configXML, boolean visAvailable) {
